98cdb18a20bf08579ffcb9fba73d22f262a200b6
Chamoda De Silva <chamoda12345678@gmail.com>
2022-06-02 11:40:36 +0530

    get and post added

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ def root():
 @app.get("/data")
 def data_get():
     return{"data":"chamoda de silva"}
+
+@app.post("/createposts")
+def create_post():
+    return{"message":"successfully poseted!"}
